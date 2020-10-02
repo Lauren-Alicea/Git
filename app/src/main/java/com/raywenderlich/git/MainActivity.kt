d2767b16ac86d23739d7c0c3d3d3eb00de02ebb5
Lauren a.m <laurenalicea1@gmail.com>
2020-10-02 10:06:18 -0500

    added activity to imagebutton

--- app/src/main/java/com/raywenderlich/git/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/git/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.raywenderlich.git
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val caterpillar_button = findViewById<ImageButton>(R.id.caterpillar_button)
+
+        caterpillar_button.setOnClickListener {
+            val intent = Intent(this, MainActivity2::class.java)
+
+            startActivity(intent)
+        }
     }
 }
